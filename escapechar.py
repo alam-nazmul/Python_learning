af8ce6dd543f1c10b25f12103f12ffe6968e426f
nirr <nirr@foundation.nazmul.com>
2022-06-30 01:41:02 +0600

    scpecial char

--- escapechar.py
+++ escapechar.py
@@ -3,3 +3,8 @@ print(splitStrings)
 
 tabbedStrings = "1\t2\t3\t4"
 print(tabbedStrings)
+
+## Use \\ for avoid confusion of especial chars when you do print 
+#print("C:\Users\nazmul\test.txt")
+
+print("C:\\Users\\nazmul\\test.txt")
