0272bd923ab718ef2354f38c678dd6c1af2250f0
alam-nazmul <alam.mdnazmul@outlook.com>
2023-01-29 16:58:44 +0600

    29-Jan-23

--- type.py
+++ type.py
@@ -3,3 +3,7 @@ print(type("Hello World"))
 print(type(17))
 print(type(3.5))
 print(type("17"))
+
+# Function allows str and int
+print(42, 17, 56, 34,11, 4.35, 32)
+print(3.4, "hello", 45)
---
+++ type_conversion.py
@@ -0,0 +1,6 @@
+print(3.14, int(3.14))
+
+print("2345", int("2345"))
+
+val = 50+5
+print("the vaule is " + str(val))
