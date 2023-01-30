bc52c98e2ae5fac5a7541b4bd0bd8034b4f6ee3b
alam-nazmul <alam.mdnazmul@outlook.com>
2023-01-30 15:01:20 +0600

    30-Jan-23

---
+++ reassignment.py
@@ -0,0 +1,12 @@
+# Updated variable will be consider
+test = 3
+test = 7
+print(test)
+
+
+x = 15
+y = x
+x = 22
+
+print(x)
+print(y)
---
+++ variables.py
@@ -0,0 +1,14 @@
+message = "What's up, Doc?"
+n = 17  # 17 = n is not possible
+pi = 3.14159
+
+print(message)
+print(n)
+print(pi)
+
+# Variables are case sensetive
+
+XYZ = 100
+xyz = "others"
+print(XYZ)
+print(xyz)
