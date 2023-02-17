1609c7c26a8e2540b96691f3de873ea65df4d9b0
alam-nazmul <alam.mdnazmul@outlook.com>
2023-02-18 00:03:11 +0600

    18-Feb-23

---
+++ loop_for2.py
@@ -0,0 +1,20 @@
+### Array to List
+for item in ['alam', 'md', 'nazmul']:
+    print(item)
+
+### Range to List
+
+for x in range(10):
+    print(x)
+
+for y in range(0, 10, 2):
+    print(y)
+
+
+### Sum of a array
+
+prices = [10, 20, 30]
+total = 0
+for price in prices:
+    total = total + price
+print(f'The sum of array is {total}')
--- mosh_test8.py
+++ mosh_test8.py
@@ -1,9 +1,16 @@
 command = ""
+started = False
 while True:
     command = input("Enter your cmd > ").lower()
     if command == "start":
+        if started:
+            print("Car has already started")
+        else:
         print("Car is started")
     elif command == "stop":
+        if not started:
+            print("Car is already stopped")
+        else:
         print("Car is stopped")
     elif command == "help":
         print("start - to start the car\nstop - to stop the car\nquit - quit this")
