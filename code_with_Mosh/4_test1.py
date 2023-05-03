18244c4d25b523a0fdd2c32c669436c87bb0bc77
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-04 03:14:44 +0600

    types

---
+++ code_with_Mosh/4_test1.py
@@ -0,0 +1,4 @@
+weight_lbs = input("Input your weight in pounds: ")
+weight_kgs = int(weight_lbs) * 0.45
+
+print(weight_kgs)
---
+++ code_with_Mosh/4_types.py
@@ -0,0 +1,4 @@
+year_of_birth = input("Birth year : ")
+age = 2023 - int(year_of_birth)
+
+print(age)
