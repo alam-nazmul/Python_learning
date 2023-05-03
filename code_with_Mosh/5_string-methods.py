ee209126a918dfd91c594e108d7f53056511b555
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-04 03:44:49 +0600

    strings_methods

---
+++ code_with_Mosh/5_string-methods.py
@@ -0,0 +1,16 @@
+course = "Python for Beginners"
+
+print(len(course))
+
+print(course.upper())
+print(course.lower())
+
+print(course.title())
+
+print(course.find("o"))
+
+print(course.replace('Beginners', 'new'))
+
+print("Python" in course)
+
+print("python" in course)
