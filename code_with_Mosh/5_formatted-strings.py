87baa2ade98f06b6a4311168e78710a3e574ef61
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-04 03:15:06 +0600

    strings

---
+++ code_with_Mosh/5_formatted-strings.py
@@ -0,0 +1,6 @@
+first_name = "Md. Nazmul"
+last_name = "Alam"
+
+msg = f'{first_name} {last_name} is a new user.'
+
+print(msg)
---
+++ code_with_Mosh/5_strings.py
@@ -0,0 +1,22 @@
+course = "Python's Course for Beginners"
+print(course)
+
+sample = '''
+Hi Nazmul,
+
+Welcome to code wish Mosh
+Thank you
+Support team
+'''
+
+print(sample)
+
+print(course[0:-2])
+print(course[2:8])
+
+another = course[:]
+print(another)
+
+
+name = "Jennofer"
+print(name[1:-1])
