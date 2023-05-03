4ccff51fbf2c4424d2076fe7ad7be8854ac5493c
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-04 01:36:21 +0600

    update

--- kodekloud/print.py
+++ kodekloud/print.py
@@ -1,2 +1,14 @@
 print("Hi, I am Nazmul")
 
+print("Hi, \nI am Nazmul")
+
+print("Hi,", "I", "am", "Nazmul")
+
+print("Hello", sep="! ", end="LOL \n")
+print("So", "enjoying python?", sep=", ", end="haha")
+
+#print('My age is ' + 25)
+
+
+
+print(1, 2, 3, 4, sep='#', end='&')
