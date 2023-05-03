8062d3d451a90b902f5e62bdfe7b5fa9b68bf436
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-04 02:27:38 +0600

    expression

--- kodekloud/print.py
+++ kodekloud/print.py
@@ -7,7 +7,7 @@ print("Hi,", "I", "am", "Nazmul")
 print("Hello", sep="! ", end="LOL \n")
 print("So", "enjoying python?", sep=", ", end="haha")
 
-#print('My age is ' + 25)
+print('My age is ' + 25)
 
 
 
