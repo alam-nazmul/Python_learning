828e271358dd3d982fc763c484e02db47d7406cc
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-05 02:58:05 +0600

    comparison operation

---
+++ code_with_Mosh/10_comparison-operator.py
@@ -0,0 +1,8 @@
+temperature = input("Enter today's temperature: ")
+temperature = int(temperature)
+if temperature > 30:
+    print("It's a hot day")
+elif temperature < 10:
+    print("It's a cold day")
+else:
+    print("It's neither hot nor cold")
