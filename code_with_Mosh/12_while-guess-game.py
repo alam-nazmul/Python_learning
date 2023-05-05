f41bf510eab0d1aa3ff7783df915bfc7adfc72a0
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-06 02:23:17 +0600

    while loop test

---
+++ code_with_Mosh/12_while-guess-game.py
@@ -0,0 +1,12 @@
+target_number = 9
+guess_limit = 3
+guess_count = 0
+
+while guess_count < guess_limit:
+    guess = int(input("Guess: "))
+    guess_count = guess_count + 1
+    if guess == target_number:
+        print("You won")
+        break
+else:
+    print("Sorry you fail")
