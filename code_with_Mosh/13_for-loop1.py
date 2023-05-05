aac420c48a553a301d7f9eadb1a113adbf79f3f6
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-06 02:23:41 +0600

    for loop

---
+++ code_with_Mosh/13_for-loop1.py
@@ -0,0 +1,6 @@
+for item in "python":
+    print(item)
+
+
+for name in ["Nazmul", "Nahin", "Ashik", "Maly"]:
+    print(name)
---
+++ code_with_Mosh/13_for-loop2.py
@@ -0,0 +1,10 @@
+for number in range(10):
+    print(number)
+
+
+for number in range(5 , 10):
+    print(number)
+
+
+for number in range(0, 10, 2):
+    print(number)
