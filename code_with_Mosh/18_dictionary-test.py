ac8afff449d2a8857e6f6cc1c437543a1fe35cf1
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-09 02:18:36 +0600

    dictionary

---
+++ code_with_Mosh/18_dictionary-test.py
@@ -0,0 +1,20 @@
+phone = input("Enter the number: ")
+
+numbers = {
+    '1': 'One',
+    '2': 'Two',
+    '3': 'Three',
+    '4': 'Four',
+    '5': 'Five',
+    '6': 'Six',
+    '7': 'Seven',
+    '8': 'Eight',
+    '9': 'Nine',
+    '0': 'Zero'
+}
+
+output = ""
+
+for number in phone:
+    output = output + numbers.get(number) + " "
+print(output)
