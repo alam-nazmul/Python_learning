848b3befe19535fbba6f7ee50482b5d94c7c3005
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-09 02:19:22 +0600

    Return statement

---
+++ code_with_Mosh/20_Return_statement.py
@@ -0,0 +1,16 @@
+def square(number):
+    return number * number
+
+
+result1 = square(3)
+print(result1)
+
+result2 = square(8)
+print(result2)
+
+
+
+def square2(number):
+    print(number*number)
+
+print(square2(4))
