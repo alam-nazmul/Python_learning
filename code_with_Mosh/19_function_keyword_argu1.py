2eca67a2c4121efd48d09d57575375704f602df7
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-09 02:21:18 +0600

    Function-Keyword-Argument

---
+++ code_with_Mosh/19_function_keyword_argu1.py
@@ -0,0 +1,8 @@
+def demo_print(first_name, last_name):
+    print(f'Hi, {first_name} {last_name}')
+    print("Welcome to Canada!!!\n")
+
+
+demo_print("Nazmul", "Alam")
+demo_print(last_name="Lota", first_name="Lutfunnahar")
+demo_print("Nazim", last_name="Uddin")
