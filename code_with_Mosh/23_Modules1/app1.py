cb706df726623a77a7404db3829c8d1850230091
alam-nazmul <alam.mdnazmul@outlook.com>
2023-05-10 02:03:56 +0600

    modules

---
+++ code_with_Mosh/23_Modules1/app1.py
@@ -0,0 +1,9 @@
+import converter
+
+print(converter.kgs_to_lbs(100))
+
+
+
+from converter import lbs_to_kgs
+
+print(lbs_to_kgs(100))
---
+++ code_with_Mosh/23_Modules2/app2.py
@@ -0,0 +1,6 @@
+from utils import find_max
+
+numbers = [10, 5, 78, 12, 85, 99, 14, 25, 36]
+
+maximum = find_max(numbers)
+print(maximum)
